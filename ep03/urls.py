0a4b18cdfae40b8d3ae34eb2b27236e75088390a
Chinseok Lee <allieuslee@gmail.com>
2017-10-27 15:27:24 +0900

    [ep03] Post List/Create API 구현

--- ep03/urls.py
+++ ep03/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^post/$', views.PostListAPIView.as_view()),
 ]
 
--- ep03/views.py
+++ ep03/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostListAPIView(APIView):
+    def get(self, request):
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.POST)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
