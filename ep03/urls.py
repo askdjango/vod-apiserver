c992240fd1876616b34cc50f77c80886e9c4d0c5
Chinseok Lee <allieuslee@gmail.com>
2017-10-27 15:36:57 +0900

    [ep03] Post Detail/Update/Delete API 구현

--- ep03/urls.py
+++ ep03/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^post/$', views.PostListAPIView.as_view()),
+    url(r'^post/(?P<pk>\d+)/$', views.PostDetailAPIView.as_view()),
 ]
 
--- ep03/views.py
+++ ep03/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
@@ -16,4 +17,28 @@ class PostListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+# /post/10/ => GET, PUT, DELETE
+class PostDetailAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Post, pk=pk)
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=204)
 
