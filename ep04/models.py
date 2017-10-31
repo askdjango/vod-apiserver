930cd9682bd61cf44f99ab14e7deee019b31e5cc
Chinseok Lee <allieuslee@gmail.com>
2017-10-31 13:52:47 +0900

    ep04 Post모델 생성 및 마이그레이션

--- ep04/models.py
+++ ep04/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    is_public = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
