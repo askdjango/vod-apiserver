db6d5c8ac85da3060c0bc05fbd3d9e2b57644e77
Chinseok Lee <allieuslee@gmail.com>
2017-11-04 16:31:46 +0900

    ep04 Post모델에 author 필드 추가

--- ep04/models.py
+++ ep04/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100)
     content = models.TextField()
     is_public = models.BooleanField(default=False)
